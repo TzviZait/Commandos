7660972451efd66b4116481053b80c14150c1464
Tzvi Zait <z0548430441@gmail.com>
2025-05-27 09:50:40 +0300

    create commando class

---
+++ Commando.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commando
+{
+    internal class Commando
+    {
+        string name;
+        string codName;
+        string[] tolls;
+        string status;
+        public Commando(string name, string codName, string[] tolls, string status)
+        {
+            this.name = name;
+            this.codName = codName;
+            this.tolls = tolls;
+            this.status = status;
+        }
+
+
+        public void Walk()
+        {
+            status = "Walking";
+            Console.WriteLine(status);
+        }
+
+        public void Hide()
+        {
+            status = "Hiding";
+            Console.WriteLine(status);
+        }
+
+
+        public void Attack()
+        {
+            Console.WriteLine(codName);
+        }
+
+    }
+}
